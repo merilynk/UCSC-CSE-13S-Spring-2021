d987700afd3af21a8791f64a5858ee3c1f5c8376
mhkuo <mhkuo@ucsc.edu>
2021-04-09 12:34:15 -0700

    finished implementing logic

--- asgn1/lrc.c
+++ asgn1/lrc.c
@@ -4,74 +4,140 @@
 
 #include "philos.h"
 
+#include <stdint.h>
 #include <stdio.h>
-#include <inttypes.h>
 #include <stdlib.h>
 
-#define FUNDS     3  // Amount of money each player starts with.
-#define MAX_ROLLS 3  // Maximum number of dice a player can do in their turn.
+#define FUNDS     3 // Amount of money each player starts with.
+#define MAX_ROLLS 3 // Maximum number of dice a player can do in their turn.
+#define PHILOS    14 // Largest number of players that can be in the game is the lentgh of philos[]
 
 // The following two functions is code provided in the assignment 1 PDF.
 
-//static inline uint8_t left(uint8_t pos, uint8_t players) {
+static inline uint8_t left(uint8_t pos, uint8_t players) {
 
-	// Returns the position of the player to the left.
-	// pos:     The position of the current player.
-	// players: The number of players in the game.
+    // Returns the position of the player to the left.
+    // pos:     The position of the current player.
+    // players: The number of players in the game.
 
-//	return ((pos + players - 1) % players);
-//}
+    return ((pos + players - 1) % players);
+}
 
-//static inline uint8_t right(uint8_t pos, uint8_t players) {
+static inline uint8_t right(uint8_t pos, uint8_t players) {
 
-	// Returns the position of the player to the right.
-	// pos:    The postition of the current player.
-	// playes: The number f players in the game.
+    // Returns the position of the player to the right.
+    // pos:    The postition of the current player.
+    // playes: The number f players in the game.
 
-//	return ((pos + 1) % players);
-//}
+    return ((pos + 1) % players);
+}
 
 int main(void) {
 
-	uint32_t bank[] = { FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS };
-	int32_t seed, players;
+    uint32_t bank[]
+        = { FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS, FUNDS };
+    int32_t seed, players;
+    uint32_t roll, left_player, right_player, pot = 0;
 
-	// The following code for taking user inputs was based off of vampires.c posted on Piazza.
+    // Next two lines is code provided in the Assignment 1 PDF.
 
-	printf("Random seed: ");
-	if ((scanf("%d", &seed) < 1) || seed < 1) {
-	    fprintf(stderr, "Pseudorandom seed must be non-negative (%d).\n", seed);
-	    return 1;
-	}
-	srandom(seed); // Set starting point for random number generator
+    typedef enum faciem { PASS, LEFT, RIGHT, CENTER } faces;
+    faces die[] = { LEFT, RIGHT, CENTER, PASS, PASS, PASS };
 
-	printf("How many players? ");
-	if ((scanf("%d", &players) < 1) || players < 1 || players > 14) {
-	    fprintf(stderr, "Number of players must be from 1 to 14.\n");
-	    return 1;
-	}
+    // The following code for taking user inputs was based off of vampires.c posted on Piazza.
 
-	uint32_t active = players;  // Number of players that have funds in their bank
+    printf("Random seed: ");
+    if ((scanf("%d", &seed) < 1) || seed < 1) {
+        fprintf(stderr, "Pseudorandom seed must be non-negative (%d).\n", seed);
+        return 1;
+    }
+    srandom(seed); // Set starting point for random number generator
 
-	for (int32_t p = 0; p < players; p += 1) {
+    printf("How many players? ");
+    if ((scanf("%d", &players) < 1) || players < 1 || players > PHILOS) {
+        fprintf(stderr, "Number of players must be from 1 to 14.\n");
+        return 1;
+    }
 
-	    if (active == 1){
-		break;  // Exit loop if only 1 active player
-	    }
+    uint32_t active = players; // Number of players that have funds in their bank
 
-	    if (bank[p] == 0) {
-		continue;  // Continue to next player if current player has no money
-	    }
+    while (active > 1) {
 
-	    uint32_t dice = bank[p];  // Number of rolls player gets is based on their funds
-	    if (dice > 3) {
-		dice = MAX_ROLLS;  // Max number of rolls player can have is three
-	    }
+        for (int32_t p = 0; p < players; p += 1) {
 
-	    for (uint32_t r = dice; r > 0; r -= 1) {
-		int roll = random() % 6;
-		printf("%d", roll);
-	    }
+            if (active == 1){
+                break;  // Exit loop if only 1 active player
+            }
 
-	}
+            if (bank[p] == 0) {
+                continue; // Continue to next player if current player has no money
+                printf("%s has no funds.\n", philosophers[p]);
+            }
+
+            uint32_t dice = bank[p]; // Number of rolls player gets is based on their funds
+            if (dice > 3) {
+                dice = MAX_ROLLS; // Max number of rolls player can have is three
+            }
+
+            printf("%s rolls...", philosophers[p]);
+
+            for (uint32_t r = dice; r > 0; r -= 1) {
+
+                roll = random() % 6; // Generate a random number
+                //printf("%d", roll);
+
+                if (die[roll] == LEFT) {
+                    bank[p] -= 1;
+
+                    if (bank[p] == 0) {
+                        active -= 1;
+                    }
+
+                    left_player = left(p, players);
+                    if (bank[left_player] == 0) {
+                        active += 1;
+                    }
+                    bank[left_player] += 1;
+                    printf(" gives $1 to %s", philosophers[left_player]);
+                }
+
+                else if (die[roll] == RIGHT) {
+                    bank[p] -= 1;
+
+                    if (bank[p] == 0) {
+                        active -= 1;
+                    }
+
+                    right_player = right(p, players);
+                    if (bank[right_player] == 0) {
+                        active += 1;
+                    }
+                    bank[right_player] += 1;
+                    printf(" gives $1 to %s", philosophers[right_player]);
+                }
+
+                else if (die[roll] == CENTER) {
+                    bank[p] -= 1;
+
+                    if (bank[p] == 0) {
+                        active -= 1;
+                    }
+                    pot += 1;
+                    printf(" puts $1 in the pot");
+                }
+
+                else if (die[roll] == PASS) {
+                    printf(" gets a pass");
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    for (int32_t p = 0; p < players; p += 1) {
+        if (bank[p] > 0) {
+            printf(
+                "%s wins the $%u pot with $%u left in the bank!\n", philosophers[p], pot, bank[p]);
+        }
+    }
 }
