7b899a449c5dfa17d1627670cc6867da105128fa
mhkuo <mhkuo@ucsc.edu>
2021-05-09 11:21:05 -0700

    i hope this works too

--- asgn5/decode.c
+++ asgn5/decode.c
@@ -2,11 +2,105 @@
 // CSE 13S Spring 2021
 // Decode
 
-#include "hamming.h"
 #include "bm.h"
 #include "bv.h"
+#include "hamming.h"
 #include "helper.h"
 
-int main(void) {
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
+#define OPTIONS "hi:o:v"
+
+// Prints help message.
+void print_help(void) {
+    printf("SYNOPSIS\n"
+	    "A Hamming(8, 4) systematic code decoder.\n\n"
+	    "USAGE\n"
+	    "./decode [-h] [-v] [-i infile] [-o outfile]\n\n"
+	    "OPTIONS\n"
+	    "  -h            Program usage and help.\n"
+	    "  -v            Print statistics of decoding to stderr.\n"
+	    "  -i infile     Input data to decode.\n"
+	    "  -o outfile    Output of decoded data.\n");
+    return;
+}
+
+int main(int argc, char **argv) {
+
+    FILE *input_file = stdin;  // default input
+    FILE *output_file = stdout;  // default output
+    struct stat statbuf;  // code from example in assignment pdf
+
+    bool verbose = false;  // true if verbose printing is enabled
+
+    // Parsing command line options
+    int opt = 0;
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1) {
+
+	switch (opt) {
+	    case 'h':
+		print_help();
+		return 0;
+		break;
+	    case 'i':
+		input_file = fopen(optarg, "rb");  // open file to read from
+		if (input_file == NULL) {
+		    fprintf(stderr, "Error: failed to open infile\n");
+		}
+		else {
+		    // get file permissions
+		    fstat(fileno(input_file), &statbuf);  // code from example in assignment pdf
+		}
+		break;
+	    case 'o':
+		output_file = fopen(optarg, "wb");  // open file to write to
+		if (output_file == NULL) {
+		    fprintf(stderr, "Error: failed to open outfile\n");
+		}
+		else {
+		    // set file permissions
+		    fchmod(fileno(output_file), statbuf.st_mode);  // code form example in assignment pdf
+		}
+		break;
+	    case 'v':
+		verbose = true;
+		break;
+	    default:
+		print_help();
+		break;
+	}
+
+    }
+
+    BitMatrix *Ht = create_H_transpose_matrix();  // create H transpose matrix
+
+    // getting input
+    int c = 0;
+    uint8_t msg1 = 0;
+    uint8_t msg2 = 0;
+
+    while ((c = fgetc(input_file)) != EOF) {
+	uint8_t code1 = lower_nibble(c);  // get upper nibble of byte
+	uint8_t code2 = upper_nibble(c);  // get lower nibble of byte
+	ham_decode(Ht, code1, &msg1);  // decode
+	ham_decode(Ht, code2, &msg2);
+	fputc(msg1, output_file);  // print to output
+	fputc(msg2, output_file);
+    }
+
+    // close files
+    if (input_file && output_file) {
+	fclose(input_file);
+	fclose(output_file);
+    }
+
+    // free memory allocated
+    bm_delete(&Ht);
+
+    return 0;
 }
--- asgn5/hamming.c
+++ asgn5/hamming.c
@@ -20,18 +20,18 @@ uint8_t ham_encode(BitMatrix *G, uint8_t msg) {
 
 HAM_STATUS ham_decode(BitMatrix *Ht, uint8_t code, uint8_t *msg) {
     BitMatrix *c = bm_from_data(code, 8);
-    bm_print(c);
+    //bm_print(c);
     BitMatrix *error = bm_multiply(c, Ht);
-    bm_print(error);
+    //bm_print(error);
     uint8_t error_syndrome = bm_to_data(error);
-    printf("error syndrome: %u\n", error_syndrome);
+    //printf("error syndrome: %u\n", error_syndrome);
     *msg = bm_to_data(c);
     if (error_syndrome == 0) {
-	printf("HAM_OK\n");
+	//printf("HAM_OK\n");
 	return HAM_OK;
     }
     else if (lookup[error_syndrome] == HAM_ERR) {
-	printf("HAM_ERR\n");
+	//printf("HAM_ERR\n");
 	return HAM_ERR;
     }
     else {
@@ -41,9 +41,9 @@ HAM_STATUS ham_decode(BitMatrix *Ht, uint8_t code, uint8_t *msg) {
 	else {
 	    bm_set_bit(c, 0, lookup[error_syndrome]);
 	}
-	printf("HAM_CORRECT\n");
-	bm_print(c);
-	printf("%u\n", bm_to_data(c));
+	//printf("HAM_CORRECT\n");
+	//bm_print(c);
+	//printf("%u\n", bm_to_data(c));
 	*msg = bm_to_data(c);
 	return HAM_CORRECT;
     }
